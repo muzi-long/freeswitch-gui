3e736ba2dd3d2c31a3cec3d12cd4c66191edfd5c
lilong <712120393@qq.com>
2019-12-31 17:47:33 +0800

    修改事件，添加esl记录通话记录

--- app/Console/Commands/asr_listen.php
+++ app/Console/Commands/asr_listen.php
@@ -30,6 +30,8 @@ class asr_listen extends Command
     protected $fs_record = '/usr/local/freeswitch/recordings/';
     protected $asr_table = null;
     protected $asr_list_table = null;
+    protected $cdr_a_leg_table = null;
+    protected $cdr_b_leg_table = null;
     public $client;
 
     /**
@@ -51,18 +53,18 @@ class asr_listen extends Command
     public function handle()
     {
         $fs = new \Freeswitchesl();
-        if (!$fs->connect(config('freeswitch.event_socket.host'), config('freeswitch.event_socket.port'), config('freeswitch.event_socket.password'))){
+        if (!$fs->connect(config('freeswitch.event_socket.host'), config('freeswitch.event_socket.port'), config('freeswitch.event_socket.password'))) {
             $this->error("ESL未连接");
             return 1;
         }
         $status = 1;
-        $fs->events('plain','CHANNEL_ANSWER CHANNEL_HANGUP RECORD_START RECORD_STOP CHANNEL_BRIDGE CHANNEL_HANGUP_COMPLETE');
-        while ($status==1){
+        $fs->events('plain', 'CHANNEL_ANSWER CHANNEL_HANGUP RECORD_START RECORD_STOP CHANNEL_BRIDGE CHANNEL_HANGUP_COMPLETE');
+        while ($status == 1) {
 
             //录音目录
-            $filepath = $this->fs_record.date('Y').'/'.date('m').'/'.date('d').'/';
+            $filepath = $this->fs_record . date('Y') . '/' . date('m') . '/' . date('d') . '/';
             if (!is_dir($filepath)) {
-                mkdir($filepath,0777,true);
+                mkdir($filepath, 0777, true);
             }
             $received_parameters = $fs->recvEvent();
             if (!empty($received_parameters)) {
@@ -70,28 +72,27 @@ class asr_listen extends Command
                 $this->autoSubmeter();
 
                 //记录日志
-                $info = $fs->serialize($received_parameters,"json");
+                $info = $fs->serialize($received_parameters, "json");
 
-                $eventname = $fs->getHeader($received_parameters,"Event-Name"); //事件名称
-                $uuid = $fs->getHeader($received_parameters,"Unique-ID");//当前信道leg的uuid
-                $CallerCallerIDNumber = $fs->getHeader($received_parameters,"Caller-Caller-ID-Number");//打电话者
-                $CallerCalleeIDNumber = $fs->getHeader($received_parameters,"Caller-Callee-ID-Number");//接电话者
+                $eventname = $fs->getHeader($received_parameters, "Event-Name"); //事件名称
+                $uuid = $fs->getHeader($received_parameters, "Unique-ID");//当前信道leg的uuid
+                $CallerCallerIDNumber = $fs->getHeader($received_parameters, "Caller-Caller-ID-Number");//打电话者
+                $CallerCalleeIDNumber = $fs->getHeader($received_parameters, "Caller-Callee-ID-Number");//接电话者
 
-                if ($eventname == 'CHANNEL_ANSWER')
-                {
+                if ($eventname == 'CHANNEL_ANSWER') {
                     //$this->info($info);
-                    $otherUuid = $fs->getHeader($received_parameters,"Other-Leg-Unique-ID");
-                    if ($otherUuid){ //被叫应答后
+                    $otherUuid = $fs->getHeader($received_parameters, "Other-Leg-Unique-ID");
+                    if ($otherUuid) { //被叫应答后
                         //设置费率
-                        $sip = Sip::with(['rate','merchant'])->where('username',$CallerCallerIDNumber)->first();
-                        if ($sip != null && isset($sip->rate->cost1) && isset($sip->merchant->id)){
+                        $sip = Sip::with(['rate', 'merchant'])->where('username', $CallerCallerIDNumber)->first();
+                        if ($sip != null && isset($sip->rate->cost1) && isset($sip->merchant->id)) {
                             $fs->bgapi("uuid_setvar {$uuid} nibble_rate {$sip->rate->cost1}");
                             $fs->bgapi("uuid_setvar {$uuid} nibble_account {$sip->merchant->id}");
                             $fs->bgapi("uuid_setvar {$uuid} nibble_increment {$sip->rate->cycle}");
                         }
                         //开启全程录音
-                        $fullfile = $filepath.'full_'.md5($otherUuid.$uuid).'.wav';
-                        if (!file_exists($fullfile)){
+                        $fullfile = $filepath . 'full_' . md5($otherUuid . $uuid) . '.wav';
+                        if (!file_exists($fullfile)) {
                             $fs->bgapi("uuid_record {$uuid} start {$fullfile} 7200"); //录音
                             $fs->bgapi("uuid_setvar {$uuid} record_file {$fullfile}");//设置录音地址变量
                             $fs->bgapi("uuid_setvar {$otherUuid} record_file {$fullfile}");//设置录音地址变量
@@ -109,8 +110,8 @@ class asr_listen extends Command
                         }
 
                         //开启分段录音A
-                        $halffile_a = $filepath.'half_'.md5($otherUuid.time().uniqid()).'.wav';
-                        $fs->bgapi("uuid_record ".$otherUuid." start ".$halffile_a." 18");
+                        $halffile_a = $filepath . 'half_' . md5($otherUuid . time() . uniqid()) . '.wav';
+                        $fs->bgapi("uuid_record " . $otherUuid . " start " . $halffile_a . " 18");
                         //记录分段录音数据
                         $this->channel[$otherUuid] = [
                             'pid' => null,
@@ -120,8 +121,8 @@ class asr_listen extends Command
                         ];
 
                         //开启分段录音B
-                        $halffile_b = $filepath.'half_'.md5($uuid.time().uniqid()).'.wav';
-                        $fs->bgapi("uuid_record ".$uuid." start ".$halffile_b." 18");
+                        $halffile_b = $filepath . 'half_' . md5($uuid . time() . uniqid()) . '.wav';
+                        $fs->bgapi("uuid_record " . $uuid . " start " . $halffile_b . " 18");
                         //记录分段录音数据
                         $this->channel[$uuid] = [
                             'pid' => null,
@@ -131,25 +132,25 @@ class asr_listen extends Command
                         ];
                     }
 
-                }elseif ($eventname == 'CHANNEL_BRIDGE'){
+                } elseif ($eventname == 'CHANNEL_BRIDGE') {
                     //$this->info($info);
 
-                }elseif ($eventname == 'RECORD_START')//开始说话
+                } elseif ($eventname == 'RECORD_START')//开始说话
                 {
 
-                }elseif ($eventname == 'RECORD_STOP')//结束说话
+                } elseif ($eventname == 'RECORD_STOP')//结束说话
                 {
-                    if (isset($this->channel[$uuid])){
+                    if (isset($this->channel[$uuid])) {
                         $halffile = $this->channel[$uuid]['record_file'];
                         if (file_exists($halffile)) {
                             //语音识别 $this->channel[$uuid]['record_file'] 文件
-                            $fileurl = str_replace('/usr/local/freeswitch',config('app.url'),$this->channel[$uuid]['record_file']);
-                            $res = file_get_contents('http://106.13.221.138:188?fileurl='.$fileurl);
-                            $result = json_decode($res,true);
-                            if ($result['status']==1){
+                            $fileurl = str_replace('/usr/local/freeswitch', config('app.url'), $this->channel[$uuid]['record_file']);
+                            $res = file_get_contents('http://106.13.221.138:188?fileurl=' . $fileurl);
+                            $result = json_decode($res, true);
+                            if ($result['status'] == 1) {
                                 $text = $result['asr'];
                                 //记录并推送websocket消息
-                                try{
+                                try {
                                     DB::table($this->asr_list_table)->insert([
                                         'id' => \Snowflake::nextId(),
                                         'pid' => $this->channel[$uuid]['pid'],
@@ -167,29 +168,27 @@ class asr_listen extends Command
                                             'uuid' => $uuid
                                         ],
                                     ];
-                                    $this->client->post('http://127.0.0.1:9501',['timeout'=>1,'form_params'=>['data'=>json_encode($pushData)]]);
-                                }catch (\Exception $exception){
-                                    Log::info("识别记录：".$exception->getMessage());
+                                    $this->client->post('http://127.0.0.1:9501', ['timeout' => 1, 'form_params' => ['data' => json_encode($pushData)]]);
+                                } catch (\Exception $exception) {
+                                    Log::info("识别记录：" . $exception->getMessage());
                                 }
                             }
                         }
                         //结束说话 后接着开启分段录音
-                        $halffile = $filepath.'half_'.md5($uuid.time().uniqid()).'.wav';
+                        $halffile = $filepath . 'half_' . md5($uuid . time() . uniqid()) . '.wav';
                         $this->channel[$uuid]['record_file'] = $halffile;
                         $this->channel[$uuid]['created_at'] = Carbon::now();
-                        $fs->bgapi("uuid_record ".$uuid." start ".$halffile." 18");
+                        $fs->bgapi("uuid_record " . $uuid . " start " . $halffile . " 18");
                     }
-                }elseif ($eventname == 'CHANNEL_HANGUP')
-                {
+                } elseif ($eventname == 'CHANNEL_HANGUP') {
                     //$this->info('CHANNEL_HANGUP事件：'.$uuid);
-                    if (isset($this->channel[$uuid])){
+                    if (isset($this->channel[$uuid])) {
                         unset($this->channel[$uuid]);
                     }
-                }elseif ($eventname == 'CHANNEL_HANGUP_COMPLETE')
-                {
+                } elseif ($eventname == 'CHANNEL_HANGUP_COMPLETE') {
                     //$this->info($info);
                     //推送websocket消息
-                    try{
+                    try {
                         $pushData = [
                             'code' => 2,
                             'msg' => '挂机',
@@ -198,10 +197,40 @@ class asr_listen extends Command
                                 'uuid' => $uuid
                             ],
                         ];
-                        $this->client->post('http://127.0.0.1:9501',['timeout'=>1,'form_params'=>['data'=>json_encode($pushData)]]);
-                    }catch (\Exception $exception){
+                        $this->client->post('http://127.0.0.1:9501', ['timeout' => 1, 'form_params' => ['data' => json_encode($pushData)]]);
+                    } catch (\Exception $exception) {
 
                     }
+                    //执行分表，记录通话记录
+                    $this->autoSubmeterCdr();
+                    $otherType = $fs->getHeader($received_parameters, "Other-Type");
+                    $thoerLegUniqueId = $fs->getHeader($received_parameters, "Other-Leg-Unique-ID");
+                    $start = $fs->getHeader($received_parameters, "variable_start_stamp");
+                    $answer = $fs->getHeader($received_parameters, "variable_answer_stamp");
+                    $end = $fs->getHeader($received_parameters, "variable_end_stamp");
+                    $cdr = [
+                        'caller_id_number' => $CallerCallerIDNumber,
+                        'destination_number' => $CallerCalleeIDNumber,
+                        'duration' => $fs->getHeader($received_parameters, "variable_duration"),
+                        'billsec' => $fs->getHeader($received_parameters, "variable_billsec"),
+                        'start_stamp' => $start ? urldecode($start) : null,
+                        'answer_stamp' => $answer ? urldecode($answer) : null,
+                        'end_stamp' => $end ? urldecode($end) : null,
+                        'hangup_cause' => $fs->getHeader($received_parameters, "variable_hangup_cause"),
+                        'record_file' => $fs->getHeader($received_parameters, "variable_record_file"),
+                        'nibble_total_billed' => (int)$fs->getHeader($received_parameters, "variable_nibble_total_billed"),
+                        'extend_content' => $fs->getHeader($received_parameters, "variable_extend_content"),
+                    ];
+                    if (empty($otherType) || $otherType == 'originatee') {
+                        $cdr['aleg_uuid'] = $uuid;
+                        $cdr['bleg_uuid'] = $thoerLegUniqueId;
+                        $table = $this->cdr_a_leg_table;
+                    } else {
+                        $cdr['aleg_uuid'] = $thoerLegUniqueId;
+                        $cdr['bleg_uuid'] = $uuid;
+                        $table = $this->cdr_b_leg_table;
+                    }
+                    DB::table($table)->insert($cdr);
                 }
             }
         }
@@ -213,15 +242,29 @@ class asr_listen extends Command
      */
     public function autoSubmeter()
     {
-        $this->asr_table = 'asr_'.date('Ym');
-        $this->asr_list_table = 'asr_list_'.date('Ym');
-        if (!Schema::hasTable($this->asr_table)){
-            DB::update("create table ".$this->asr_table." like asr");
+        $this->asr_table = 'asr_' . date('Ym');
+        $this->asr_list_table = 'asr_list_' . date('Ym');
+        if (!Schema::hasTable($this->asr_table)) {
+            DB::update("create table " . $this->asr_table . " like asr");
         }
-        if (!Schema::hasTable($this->asr_list_table)){
-            DB::update("create table ".$this->asr_list_table." like asr_list");
+        if (!Schema::hasTable($this->asr_list_table)) {
+            DB::update("create table " . $this->asr_list_table . " like asr_list");
         }
+    }
 
+    /**
+     * cdr自动分表
+     */
+    public function autoSubmeterCdr()
+    {
+        $this->cdr_a_leg_table = 'cdr_a_leg_' . date('Ym');
+        $this->cdr_b_leg_table = 'cdr_b_leg_' . date('Ym');
+        if (!Schema::hasTable($this->cdr_a_leg_table)) {
+            DB::update("create table " . $this->cdr_a_leg_table . " like cdr_a_leg");
+        }
+        if (!Schema::hasTable($this->cdr_b_leg_table)) {
+            DB::update("create table " . $this->cdr_b_leg_table . " like cdr_b_leg");
+        }
     }
 
 }
