431e662cba4a5d17d9e6698c5ce57082d48d312c
lilong <712120393@qq.com>
2020-05-14 11:56:25 +0800

    修改esl:cdr监控名称

--- app/Console/Commands/eslCdr.php
+++ app/Console/Commands/eslCdr.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redis;
 
-class asrCdr extends Command
+class eslCdr extends Command
 {
     /**
      * The name and signature of the console command.
