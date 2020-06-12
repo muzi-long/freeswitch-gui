d5e912cb0855bc9dda87fcf8480c01aa94adcda7
lilong@dgg.net <123456789>
2020-06-12 11:27:32 +0800

    更改fs连接为默认连接

--- config/freeswitch.php
+++ config/freeswitch.php
@@ -37,8 +37,8 @@ return [
 
     'esl' => [
         'host' => '127.0.0.1',
-        'password' => 'dgg@1234.',
-        'port' => 8022,
+        'password' => 'ClueCon',
+        'port' => 8021,
     ],
 
     //队列响铃模式
